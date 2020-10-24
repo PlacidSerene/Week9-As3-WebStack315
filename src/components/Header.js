d1a7142f492cf5c43e6922fde3c9f704b0be2474
Daniel Nguyen <dung.nguyen@jessup.edu>
2020-10-24 13:35:30 -0700

    assignment commit

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./componentStyle.css";
+export default function Header() {
+  return <header></header>;
+}
---
+++ src/components/SubContents.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SubContents() {
+  return <div className="subDiv"></div>;
+}
